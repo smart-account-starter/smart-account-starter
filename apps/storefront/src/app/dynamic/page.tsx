01956edccb724ef5dd873277e9059255d4adb198
cryptoB0T <cryptobotmachine@gmail.com>
2024-10-05 11:36:19 -0400

    Refactor DynamicWidget to DynamicEmbeddedWidget with modified styling in dynamic page component.

--- apps/storefront/src/app/dynamic/page.tsx
+++ apps/storefront/src/app/dynamic/page.tsx
@@ -1,27 +1,36 @@
-'use client'
-import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { useEffect } from 'react';
+"use client"
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+  DynamicEmbeddedWidget,
+} from "@dynamic-labs/sdk-react-core"
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum"
+import { useEffect } from "react"
 
 function App() {
   useEffect(() => {
     // Automatically click the Dynamic Widget button on page load
-    const dynamicButton = document.querySelector('.dynamic-widget-button') as HTMLButtonElement;
+    const dynamicButton = document.querySelector(
+      ".dynamic-widget-button"
+    ) as HTMLButtonElement
     if (dynamicButton) {
-      dynamicButton.click();
+      dynamicButton.click()
     }
-  }, []);
+  }, [])
 
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_LABS_ENVIRONMENT_ID ?? '',
-        walletConnectors: [ EthereumWalletConnectors ],
+        environmentId:
+          process.env.NEXT_PUBLIC_DYNAMIC_LABS_ENVIRONMENT_ID ?? "",
+        walletConnectors: [EthereumWalletConnectors],
       }}
     >
-      <DynamicWidget />
+      <div className="w-1/2 mx-auto">
+        <DynamicEmbeddedWidget style={{ minHeight: 500 }} />
+      </div>
     </DynamicContextProvider>
-  );
+  )
 }
 
-export default App;
+export default App
