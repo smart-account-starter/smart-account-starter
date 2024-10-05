46285f28e96edcff3f828c9838f6192803d46432
cryptoB0T <cryptobotmachine@gmail.com>
2024-10-05 12:20:07 -0400

    Update import paths in ClientSideMagicDemo for CSS and polyfills.

--- apps/storefront/src/app/magic-demo/components/ClientSideMagicDemo.tsx
+++ apps/storefront/src/app/magic-demo/components/ClientSideMagicDemo.tsx
@@ -2,8 +2,8 @@
 
 import { StrictMode } from "react"
 import dynamic from 'next/dynamic'
-import "./index.css"
-import "./polyfills.ts"
+import "../index.css"
+import "../polyfills.ts"
 import "react-toastify/dist/ReactToastify.css"
 
 const DynamicApp = dynamic(() => import('../App.tsx'), { ssr: false })
